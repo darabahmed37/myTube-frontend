c51d5c511cecdbb1a8356e4fadceb02e81064b43
Darab Ahmed <darabahmed37@gmail.com>
2022-08-06 22:39:03 +0500

    Coverted Css styles to emotion components

---
+++ src/layouts/Homestyles.tsx
@@ -0,0 +1,10 @@
+import {styled} from "@mui/material";
+
+export const HomeContainer = styled("div")(({theme}) => ({
+  background: "#1e1e1e",
+  height: "100vh",
+  display: "flex",
+  minWidth: "1280px",
+  alignItems: "center",
+}))
+
