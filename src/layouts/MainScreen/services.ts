96f3bc8c09e41723fb2822f141d82439bfbf5c98
Darab Ahmed <darabahmed37@gmail.com>
2022-08-26 10:59:30 +0500

    Fixed Video Deleted Issue

--- src/layouts/MainScreen/services.ts
+++ src/layouts/MainScreen/services.ts
@@ -6,12 +6,25 @@ import { getPlayListById } from "api/youtube";
 export async function getPlaylistByIdAction(id: string) {
 	const youTubePlayLists: IYouTubePlayListItems = (await getPlayListById(id)).data;
 
-	const temp: VideoCardProps[] = youTubePlayLists.items.map((item) => ({
-		title: item.snippet.title,
+	const temp: VideoCardProps[] = [];
+	youTubePlayLists.items.forEach((item: any) => {
+		if (Object.keys(item.snippet.thumbnails).length !== 0) {
+			temp.push({
+				title: item.snippet.title,
+				thumbnails: item.snippet.thumbnails,
+				id: item.id,
+				description: item.snippet.description,
+				videoId: item.snippet.resourceId.videoId,
+			});
+		}
+	});
+	return temp;
+}
+
+/*
+title: item.snippet.title,
 		thumbnails: item.snippet.thumbnails,
 		id: item.id,
 		description: item.snippet.description,
 		videoId: item.snippet.resourceId.videoId,
-	}));
-	return temp;
-}
+ */
