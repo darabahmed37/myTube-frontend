b3247ce3ab1500b0b8013fda98c7ff6ee0e610c3
Darab Ahmed <darabahmed37@gmail.com>
2022-08-30 17:33:36 +0500

    Chart Handled

--- src/pages/Statistics/emotion.ts
+++ src/pages/Statistics/emotion.ts
@@ -2,17 +2,21 @@ import { Box, styled } from "@mui/material";
 import { H1, H3 } from "elements/Typography";
 
 export const TimeChart = styled(Box)`
-	margin-top: 4em;
+  margin-top: 4em;
+  display: flex;
+  width: 100%;
+
+  align-items: center;
 `;
 
 export const StatH1 = styled(H1)`
-	margin-left: 2em;
-	margin-top: 2em;
+  margin-left: 2em;
+  margin-top: 2em;
 `;
 
 export const Graph = styled(Box)`
-	${H3} {
-		margin-top: 2em;
-		margin-left: 3.5em;
-	}
+  ${H3} {
+    margin-top: 2em;
+    margin-left: 3.5em;
+  }
 `;
--- src/pages/Statistics/index.tsx
+++ src/pages/Statistics/index.tsx
@@ -1,5 +1,17 @@
 import React, { FC, useEffect } from "react";
-import { CartesianGrid, Line, LineChart, ReferenceLine, Tooltip, XAxis, YAxis } from "recharts";
+import {
+	CartesianGrid,
+	Cell,
+	Line,
+	LineChart,
+	Pie,
+	PieChart,
+	ReferenceLine,
+	ResponsiveContainer,
+	Tooltip,
+	XAxis,
+	YAxis,
+} from "recharts";
 import { ITimer } from "types/Timer";
 import { convertToTime, getAllTagsAction, getPreviousTimersAction } from "pages/Statistics/service";
 import { Graph, StatH1, TimeChart } from "pages/Statistics/emotion";
@@ -9,12 +21,29 @@ import { ITags } from "types/Tags";
 const Statistics: FC = () => {
 	const [timeData, setTimeData] = React.useState<ITimer[]>([]);
 	const [tagsData, setTagsData] = React.useState<ITags[]>([]);
+	const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#ab47bc","#f73378","#3f51b5"];
+
+	const RADIAN = Math.PI / 180;
+	const renderCustomizedLabel = (props: any) => {
+		const { cx, cy, midAngle, innerRadius, outerRadius, name } = props;
+		const radius = innerRadius + (outerRadius - innerRadius) + 30;
+		const x = cx + radius * Math.cos(-midAngle * RADIAN);
+		const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+		return (
+			<text x={x} y={y} fill="black" textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">
+				{name}
+			</text>
+		);
+	};
+
 	useEffect(() => {
 		getPreviousTimersAction().then((output) => {
 			setTimeData(output);
 		});
 		getAllTagsAction().then((output) => {
 			setTagsData(output);
+			console.log(output);
 		});
 	}, []);
 
@@ -42,13 +71,26 @@ const Statistics: FC = () => {
 				<Graph>
 					<H3>User Tags</H3>
 					<TimeChart>
-						<LineChart width={600} height={300} data={tagsData}>
-							<Line type="monotone" dataKey="count" stroke="#8884d8" />
-							<CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-							<XAxis dataKey="tag" />
-							<YAxis />
-							<Tooltip />
-						</LineChart>
+						<ResponsiveContainer height={300} width={"100%"}>
+							<PieChart width={500} height={700}>
+								<Pie
+									data={tagsData}
+									labelLine={true}
+									dataKey="count"
+									nameKey="tag"
+									cx="50%"
+									cy="50%"
+									outerRadius={"70%"}
+									fill="#8884d8"
+									label={renderCustomizedLabel}
+								>
+									{tagsData.map((entry, index) => (
+										<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+									))}
+								</Pie>
+								<Tooltip />
+							</PieChart>
+						</ResponsiveContainer>
 					</TimeChart>
 				</Graph>
 			) : (
